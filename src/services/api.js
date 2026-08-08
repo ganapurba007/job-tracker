4889a0fe102ab2b88180626cd78b612e1443be76
ganapurba007 <ganapurbakusuma86@gmail.com>
2026-08-08 16:01:31 +0700

    fix: edit api URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
-// const API_BASE_URL = 'http://127.0.0.1:8000/api'
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://job-tracker-api-ruby.vercel.app/api'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
